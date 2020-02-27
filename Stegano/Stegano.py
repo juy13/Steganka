cea2affe3623b2135de8cde45d9ccb799b944c28
Julian <julyk.volyansky13@yandex.ru>
2020-02-27 11:58:33 +0300

    Added new steps

--- Stegano/Stegano.py
+++ Stegano/Stegano.py
@@ -2,6 +2,8 @@ import os
 import sys
 from ctypes import *
 import soundfile as sf
+import numpy as np
+import math
 
 def first_step(m):
 	a = 1.1
@@ -12,18 +14,57 @@ def first_step(m):
 	Lm = 8 * Nm
 	d0 = 20
 	d1 = 30
-	C0 = [0 for x in range(d0)]
-	C0 = C0 + C
-	C1 = [0 for x in range(d1)]
+	C0 = np.array([0 for x in range(d0)])
+	C0 = np.concatenate((C0, C), axis=0) * a
+	C1 = np.array([0 for x in range(d1)])
+	C1 = np.concatenate((C1, C), axis=0) * a
 
+	SC0 = np.array([0 for x in range(rowsC)])	
 
-def stack(A, B):
-	return A.extend(B)
+	for i in range(rowsC):
+		SC0[i] = C[i] + C0[i]
+
+	SC1 = np.array([0 for x in range(rowsC)])	
+
+	for i in range(rowsC):
+		SC1[i] = C[i] + C1[i]
+
+	Nb = math.floor(rowsC/Lm)
+
+	U = 1
+	tau = 20
+	T = Nb - tau
+	
+	fp, u, fz = module_1(tau, T)
+	
+	
+
+def module_1(tau_c, T_c):
+	fp = np.zeros(tau_c)
+	for n in range(tau_c):
+		if tau_c > 0:
+			fp[n] = ((n - tau_c)/tau_c)
+		if tau_c == 0:
+			fp[0] = 0
+
+	u = np.array(1 for i in raange(T_c - tau_c))
+
+	fz = np.zeros(2 * tau_c)
+	for n in range(2 * tau_c):
+		if tau_c > 0:
+			fz[n] = (i - tau_c)/(-tau_c)
+		if tau_c == 0:
+			fz[0] = 0
+
+	return fp, u, fz
+
+		
+
 
 
 
 filename = '90.wav'
-data, fd = sf.read('90.wav')
+#data, fd = sf.read('90.wav')
 #sf.write('901.wav', data, fd)
 workLib = cdll.LoadLibrary("Steganka.dll")
 filename_in = c_char * len(filename)											
